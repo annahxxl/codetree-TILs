8470905eef5b787ae95c49a59d2ce1faa3f66bc0
Hanna Lee <8annahxxl@gmail.com>
2024-04-30 16:45:25 +0900

    Title: 갈 수 있는 곳들, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240430/갈 수 있는 곳들/places-can-go.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Main {
+    static int n, k, answer;
+    static int[][] grid;
+    static boolean[][] visited;
+    static Queue<int[]> q = new LinkedList<>();
+    static int[][] dir = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
+
+    static void bfs() {
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int x = cur[0], y = cur[1];
+            
+            for (int i = 0; i < dir.length; i++) {
+                int[] d = dir[i];
+                int nx = x + d[0], ny = y + d[1];
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n && grid[nx][ny] == 0 && visited[nx][ny] == false) {
+                    visited[nx][ny] = true;
+                    answer++;
+                    q.offer(new int[]{nx, ny});
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        k = sc.nextInt();
+
+        grid = new int[n][n];
+        visited = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < k; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            q.offer(new int[]{x, y});
+        }
+
+        bfs();
+
+        System.out.println(answer);
+    }
+}
