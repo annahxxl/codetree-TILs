fcd622411a311fcec69c76864c9bfcf439668783
Hanna Lee <8annahxxl@gmail.com>
2024-04-30 15:40:48 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240430/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    static int n, m, answer;
+    static int[][] grid;
+    static boolean[][] visited;
+    static int[][] dir = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
+
+    static void dfs(int x, int y) {
+        for (int i = 0; i < dir.length; i++) {
+            int[] d = dir[i];
+            int nx = x + d[0], ny = y + d[1];
+            
+            if (nx >= 0 && nx < n && ny >= 0 && nx < m && grid[nx][ny] == 1 && visited[nx][ny] == false) {
+                visited[nx][ny] = true;
+                dfs(nx, ny);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        grid = new int[n][m];
+        visited = new boolean[n][m];
+
+        visited[0][0] = true;
+
+        dfs(0, 0);
+
+        System.out.println(visited[n - 1][m - 1] == true ? 1 : 0);
+    }
+}
