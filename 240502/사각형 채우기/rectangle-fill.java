bb2cdc4cdbb0ed5d2f3e2079b58516b96305ccd9
Hanna Lee <8annahxxl@gmail.com>
2024-05-02 14:27:35 +0900

    Title: 사각형 채우기, Time: 166ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240502/사각형 채우기/rectangle-fill.java
+++ 240502/사각형 채우기/rectangle-fill.java
@@ -7,11 +7,12 @@ public class Main {
         int n = sc.nextInt();
         int[] dp = new int[1001];
 
+        dp[0] = 1; // 타일을 전혀 놓지 않는 경우 1가지
         dp[1] = 1;
-        dp[2] = 2;
+        // dp[2] = 2;
 
         for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007; // 끝에 세로 1개 붙이는 경우, 끝에 가로 2개 붙이는 경우
         }
 
         System.out.println(dp[n]);
