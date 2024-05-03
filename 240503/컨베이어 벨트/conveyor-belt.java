74bd2fae3e71dcaaa5401dfe6e35ff6fb0eb6c8b
Hanna Lee <8annahxxl@gmail.com>
2024-05-03 14:16:49 +0900

    Title: 컨베이어 벨트, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240503/컨베이어 벨트/conveyor-belt.java
+++ 240503/컨베이어 벨트/conveyor-belt.java
@@ -7,37 +7,30 @@ public class Main {
         int n = sc.nextInt();
         int t = sc.nextInt();
 
-        int[][] arr = new int[2][n];
+        int[] u = int[n];
+        int[] d = int[n];
 
-        for (int i = 0; i < n; i++) {
-            arr[0][i] = sc.nextInt();
-        }
-        for (int i = n - 1; i >= 0; i--) {
-            arr[1][i] = sc.nextInt();
-        }
+        for(int i = 0; i < n; i++)
+            u[i] = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            d[i] = sc.nextInt();
 
+        for(int i = 0; i < t; i++) {
+            int tmp = u[n - 1];
 
-        for (int i = 0; i < t; i++) {
-            int tmp1 = arr[0][n - 1];
-            for (int j = n - 1; j > 0; j--) {
-                arr[0][j] = arr[0][j - 1];
-            }
+            for(int i = n - 1; i > 0; i--) 
+                u[i] = u[i - 1];
+            u[0] = d[n - 1];
 
-            int tmp2 = arr[1][0];
-            for (int j = 0; j < n - 1; j++) {
-                arr[1][j] = arr[1][j + 1];
-            }
-
-            arr[0][0] = tmp2;
-            arr[1][n - 1] = tmp1;
+            for(int i = n - 1; i > 0; i--)
+                d[i] = d[i - 1];
+            d[0] = tmp;
         }
 
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[0][i] + " ");
-        }
+        for(int i = 0; i < n; i++)
+            System.out.print(u[i] + " ");
         System.out.println();
-        for (int i = n - 1; i >= 0; i--) {
-            System.out.print(arr[1][i] + " ");
-        }
+        for(int i = 0; i < n; i++)
+            System.out.print(d[i] + " ");
     }
 }
