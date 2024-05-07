ab4c33cd5d441d2f5dbc2d43ce1482ddc8d3df27
Hanna Lee <8annahxxl@gmail.com>
2024-05-07 10:09:20 +0900

    Title: 나이트, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240507/나이트/knight-movements.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int r1 = sc.nextInt(), c1 = sc.nextInt(), r2 = sc.nextInt(),c2 = sc.nextInt();
+
+        int[][] step = new int[n + 1][n + 1];
+        boolean[][] visited = new boolean[n + 1][n + 1];
+        int[][] dir = new int[][] { {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}, {1, 2}, {2, 1}, {2, -1}, {1, -2} };
+        Queue<int[]> q = new LinkedList<>();
+
+        visited[r1][c1] = true;
+        q.add(new int[] {r1, c1});
+
+        while(!q.isEmpty()) {
+            int[] cur = q.poll();
+            int r = cur[0], c = cur[1];
+
+            for(int i = 0; i < dir.length; i++) {
+                int nr = r + dir[i][0], nc = c + dir[i][1];
+
+                if(nr >= 1 && nr <= n && nc >= 1 && nc <= n && visited[nr][nc] == false) {
+                    step[nr][nc] = step[r][c] + 1;
+                    visited[nr][nc] = true;
+                    q.add(new int[] {nr, nc});
+                }
+            }
+        }
+        
+        System.out.println(step[r2][c2] == 0 ? -1 : step[r2][c2]);
+    }
+}
