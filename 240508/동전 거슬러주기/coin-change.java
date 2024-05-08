8ed5499ac1d8cec5ca360061b09f84ac12414382
Hanna Lee <8annahxxl@gmail.com>
2024-05-08 21:35:44 +0900

    Title: 동전 거슬러주기, Time: 178ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240508/동전 거슬러주기/coin-change.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] coin = new int[n];
+        for(int i = 0; i < n; i++)
+            coin[i] = sc.nextInt();
+
+        int[] dp = new int[m + 1];
+        for(int i = 0; i <= m; i++)
+            dp[i] = Integer.MAX_VALUE;
+
+        dp[0]= 0;
+
+        for(int i = 1; i <= m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i - coin[j] >= 0) {
+                    if(dp[i - coin[j]] != Integer.MAX_VALUE)
+                        dp[i] = Math.min(dp[i], dp[i - coin[j]] + 1);
+                }
+            }
+        }
+
+        System.out.println(dp[m] == Integer.MAX_VALUE ? -1 : dp[m]);
+    }
+}
