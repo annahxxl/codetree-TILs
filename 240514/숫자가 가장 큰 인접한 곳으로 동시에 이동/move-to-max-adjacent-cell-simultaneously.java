579620dd9c85edda50d43a581257e74b2b949e38
Hanna Lee <8annahxxl@gmail.com>
2024-05-14 15:57:41 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240514/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int t = sc.nextInt();
+        int[][] grid = new int[n][n];
+        int[][] cnt = new int[n][n];
+        int[][] ncnt = new int[n][n];
+
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
+        
+        for(int i = 0; i < m; i++) {
+            int r = sc.nextInt() - 1;
+            int c = sc.nextInt() - 1;
+            cnt[r][c] += 1;
+        }
+
+        int[][] dir = new int[][] { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
+    
+    ///
+
+        for(int ct = 0; ct < t; ct++) {
+
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    if(cnt[i][j] == 1) {
+                        int max = 0, mr = 0, mc = 0;
+
+                        for(int k = 0; k < dir.length; k++) {
+                            int nr = i + dir[k][0];
+                            int nc = j + dir[k][1];
+
+                            if(nr >= 0 && nr < n && nc >= 0 && nc < n && max < grid[nr][nc]) {
+                                max = grid[nr][nc];
+                                mr = nr;
+                                mc = nc;
+                            }
+                        }
+
+                        ncnt[i][j]++;
+                    }
+                }
+            }
+
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    if(ncnt[i][j] == 1) {
+                        cnt[i][j] = 1;
+                    } else {
+                        cnt[i][j] = 0;
+                    }
+                }
+            }
+        }
+        
+        int answer = 0;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(ncnt[i][j] == 1)
+                    answer++;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
