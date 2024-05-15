5bf438c5bf601921f4ac076ee6938ba21db71379
Hanna Lee <8annahxxl@gmail.com>
2024-05-15 20:51:06 +0900

    Title: 부분 수열의 합이 m, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

--- 240515/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.java
+++ 240515/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int MAX_VALUE = 10000; 
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[] nums = new int[n];
@@ -13,20 +14,15 @@ public class Main {
             nums[i] = sc.nextInt();
 
         for(int i = 1; i <= m; i++)
-            dp[i] = -1;
+            dp[i] = MAX_VALUE;
         
         for(int i = 0; i < n; i++) {
             for(int j = m; j >= 0; j--) {
-                if(j - nums[i] == 0) {
-                    dp[j] = 1;
-                    continue;
-                }
-                if(j - nums[i] > 0 && dp[j - nums[i]] != -1) {
-                    dp[j] = dp[j] == -1 ? dp[j - nums[i]] + 1 : Math.min(dp[j], dp[j - nums[i]] + 1);
-                }
+                if(j >= nums[i])
+                    dp[j] =  Math.min(dp[j], dp[j - nums[i]] + 1);
             }
         } 
 
-        System.out.println(dp[m]);
+        System.out.println(dp[m] == MAX_VALUE ? -1 : dp[m]);
     }
 }
