d9ef28ac68b2fdeffe0502640e2b11c7533b429f
Hanna Lee <8annahxxl@gmail.com>
2024-05-17 23:14:58 +0900

    Title: 계단 수, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240517/계단 수/stair-number.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    static final int MOD = 1000000007;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] dp = new int[n + 1][10]; // i번째까지 선택, 마지막으로 선택한 수가 j
+
+        for(int i = 1; i <= 9; i++)
+            dp[1][i] = 1;
+        
+        for(int i = 2; i <= n; i++) {
+            for(int j = 0; j <= 9; j++) {
+                if(j == 0) {
+                    dp[i][j] = dp[i - 1][j + 1] % MOD;
+                } else if(j == 9) {
+                    dp[i][j] = dp[i - 1][j - 1] % MOD;
+                } else {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD;
+                }
+            }
+        }
+
+        int answer = 0;
+
+        for(int i = 0; i <= 9; i++)
+            answer += dp[n][i];
+        
+        System.out.println(answer);
+    }
+}
