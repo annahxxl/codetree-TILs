6baeeed91860549f993f20dfaebca167e4141f6d
Hanna Lee <8annahxxl@gmail.com>
2024-05-17 11:16:00 +0900

    Title: 정수 사각형 최소 합, Time: 334ms, Memory: 21MB, Status: Passed - CodeTree

---
+++ 240517/정수 사각형 최소 합/minimum-sum-path-in-square.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] grid = new int[n][n];
+
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
+
+        int[][] dp = new int[n][n];
+        dp[0][n - 1] = grid[0][n - 1];
+
+        for(int i = 1; i < n; i++)
+            dp[i][n - 1] = dp[i - 1][n - 1] + grid[i][n - 1];
+
+        for(int i = n - 2; i >= 0; i--)
+            dp[0][i] = dp[0][i + 1] + grid[0][i];
+
+        for(int i = 1; i < n; i++)
+            for(int j = n - 2; j >= 0; j--)
+                dp[i][j] = Math.min(dp[i - 1][j] + grid[i][j], dp[i][j + 1] + grid[i][j]);
+        
+
+        System.out.println(dp[n - 1][0]);
+    }
+}
