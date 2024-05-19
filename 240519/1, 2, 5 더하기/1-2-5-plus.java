9c9c9141d1207efdaf10eb25b4adb896b49f1287
Hanna Lee <8annahxxl@gmail.com>
2024-05-19 20:41:31 +0900

    Title: 1, 2, 5 더하기, Time: 144ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240519/1, 2, 5 더하기/1-2-5-plus.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] dp = new int[n + 1];
+        int[] nums = new int[] {1, 2, 5};
+
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[5] = 1;
+
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j < nums.length; j++) {
+                if(i >= nums[j]) {
+                    dp[i] += dp[i - nums[j]]; 
+                }
+            }
+        }
+
+        System.out.println(dp[n]);        
+    }
+}
