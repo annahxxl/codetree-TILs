ff113c470d123dd591d2df833d13cf7167a9c079
Hanna Lee <8annahxxl@gmail.com>
2024-05-19 20:42:24 +0900

    Title: 1, 2, 5 더하기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240519/1, 2, 5 더하기/1-2-5-plus.java
+++ 240519/1, 2, 5 더하기/1-2-5-plus.java
@@ -15,7 +15,7 @@ public class Main {
         for(int i = 0; i <= n; i++) {
             for(int j = 0; j < nums.length; j++) {
                 if(i >= nums[j]) {
-                    dp[i] += dp[i - nums[j]]; 
+                    dp[i] = (dp[i] + dp[i - nums[j]]) % 10007; 
                 }
             }
         }
