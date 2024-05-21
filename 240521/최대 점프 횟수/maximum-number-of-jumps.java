120cdbb16f653415119b502d25b243ad6d14f4aa
Hanna Lee <8annahxxl@gmail.com>
2024-05-21 15:10:42 +0900

    Title: 최대 점프 횟수, Time: 164ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240521/최대 점프 횟수/maximum-number-of-jumps.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i = 0; i < n; i++)
+            nums[i] = sc.nextInt();
+
+        int[] dp = new int[n]; // 마지막 숫자가 idx번째 숫자
+
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(j + nums[j] <= i) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int answer = 0;
+
+        for(int i = 0; i < n; i++)
+            answer = Math.max(answer, dp[i]);
+
+        System.out.println(answer);
+    }
+}
