431a03bd48f53a61763a8ab62e2cf8ddb45911e2
Hanna Lee <8annahxxl@gmail.com>
2024-05-22 15:16:49 +0900

    Title: 최대 동전 거슬러주기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240522/최대 동전 거슬러주기/max-coin-change.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] coins = new int[n];
+        for(int i = 0; i < n; i++)
+            coins[i] = sc.nextInt();
+        
+        int[] dp = new int[m + 1];
+        for(int i = 0; i <= m; i++)
+            dp[i] = -1;
+        for(int i = 0; i < n; i++)
+            dp[coins[i]] = 1;
+        
+        for(int i = 1; i <= m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i - coins[j] >= 0 && dp[i - coins[j]] != -1) {
+                    dp[i] = Math.max(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+
+        System.out.println(dp[m]);
+    }
+}
