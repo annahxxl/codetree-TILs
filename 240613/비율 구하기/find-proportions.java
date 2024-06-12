152bc4e723ab162bddc6298f0336b512647651f9
Hanna Lee <8annahxxl@gmail.com>
2024-06-13 08:23:38 +0900

    Title: 비율 구하기, Time: 617ms, Memory: 23MB, Status: Wrong Answer - CodeTree

--- 240613/비율 구하기/find-proportions.java
+++ 240613/비율 구하기/find-proportions.java
@@ -6,18 +6,15 @@ public class Main {
         int n = sc.nextInt();
 
         Map<String, Integer> m = new TreeMap<>();
-        int total = 0;
 
         for(int i = 0; i < n; i++) {
             String k = sc.next();
             m.put(k, m.getOrDefault(k, 0) + 1);
-            total++;
         }
 
         for(Map.Entry<String, Integer> e : m.entrySet()) {
-            double p = (e.getValue() * 100.0) / total;
-            String fp = String.format("%.4f", p);
-            System.out.println(e.getKey() + " " + fp);
+            double ratio = e.getValue() * 100 / n;
+            System.out.printf("%s %.4f\n", e.getKey(), ratio);
         }
     }
 }
