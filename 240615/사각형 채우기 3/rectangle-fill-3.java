9374bac32da1e2377a4fa53c556374b8092fb71c
Hanna Lee <8annahxxl@gmail.com>
2024-06-15 04:29:31 +0900

    Title: 사각형 채우기 3, Time: 167ms, Memory: 10MB, Status: Passed - CodeTree

--- 240615/사각형 채우기 3/rectangle-fill-3.java
+++ 240615/사각형 채우기 3/rectangle-fill-3.java
@@ -7,12 +7,12 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         
-        int[] dp = new int[1001];
+        long[] dp = new long[1001];
         dp[0] = 1;
         dp[1] = 2;
         
         for(int i = 2; i <= n; i++) {
-            dp[i] = (dp[i - 1] * 2) + (dp[i - 2] * 3) % MOD;
+            dp[i] = ((dp[i - 1] * 2) + (dp[i - 2] * 3)) % MOD;
             for(int j = i - 3; j >= 0; j--) 
                 dp[i] = (dp[i] + (dp[j] * 2)) % MOD;
         }
