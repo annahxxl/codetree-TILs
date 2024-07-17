6d3939b80584038567fbd486cbc561c07abbc89c
Hanna Lee <8annahxxl@gmail.com>
2024-07-17 09:17:51 +0900

    Title: 서로 다른 BST 개수 세기, Time: 141ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240717/서로 다른 BST 개수 세기/number-of-unique-bst.java
+++ 240717/서로 다른 BST 개수 세기/number-of-unique-bst.java
@@ -1,27 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
-    private static int n;
-    private static int[] dp; // memoization
-
-    private static int bt(int n) {
-        if(n == 1) return 1;
-
-        if(dp[n] != 0) return dp[n];
-
-        int cnt = 0;
-        for(int i = 0; i < n; i++) 
-            cnt += bt(i) * bt(n - i - 1);
-        dp[n] = cnt;
-
-        return cnt;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        dp = new int[n + 1];
+        int n = sc.nextInt();
+        int[] dp = new int[n + 1];
+
         dp[0] = 1;
-        System.out.println(bt(n));
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++) {
+            int cnt = 0;
+            for(int j = 0; j < n; j++) {
+                cnt += dp[j] * dp[n - j - 1];
+            }
+            dp[i] = cnt;
+        }
+
+        System.out.println(dp[n]);
     }
 }
