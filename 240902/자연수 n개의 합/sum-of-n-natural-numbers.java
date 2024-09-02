861a1c452dfe19fe4fb4872161e0f4204843e4b8
Hanna Lee <8annahxxl@gmail.com>
2024-09-02 12:38:14 +0900

    Title: 자연수 n개의 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240902/자연수 n개의 합/sum-of-n-natural-numbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long s = sc.nextInt();
+
+        long l = 1, r = 2000000000;
+        long answer = -1;
+
+        while(l <= r) {
+            long mid = (l + r) / 2;
+
+            if(mid * (mid + 1) / 2 <= s) {
+                l = mid + 1;
+                answer = Math.max(answer, mid);
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
