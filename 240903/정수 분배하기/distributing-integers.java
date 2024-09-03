d9021a53c818a1ce38b6144ef21eb4136a89c4d7
Hanna Lee <8annahxxl@gmail.com>
2024-09-03 22:11:04 +0900

    Title: 정수 분배하기, Time: 151ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240903/정수 분배하기/distributing-integers.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+    static int n, m;
+    static int[] nums;
+
+    static boolean isPossible(int max) {
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(nums[i] < max)
+                return false;
+            
+            cnt += (nums[i] / max);
+        }
+
+        return cnt >= m;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        nums = new int[n];
+        for(int i = 0; i < n; i++)
+            nums[i] = sc.nextInt();
+        
+        int ans = -1;
+        int l = 1;
+        int r = 100000;
+
+        while(l <= r) {
+            int mid = (l + r) / 2;
+
+            if(isPossible(mid)) {
+                l = mid + 1;
+                ans = Math.max(ans, mid);
+            } else {
+                r = mid - 1;
+            }
+        }
+        
+        System.out.println(ans);
+    }
+}
